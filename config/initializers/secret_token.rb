3282be2ac86db734ff8dd5393ccff024fc90064f
nyghtowl <warrick.melanie@gmail.com>
2013-07-12 16:36:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarkovRails::Application.config.secret_token = '2282a6ab9b85a7fc5948e060900d06be5fa40efba44136a4b44a58edde04b0aa9c89bec8a127f763628fa3e994597c66b4a7706ffa344e20c6ad7c511f12a660'
