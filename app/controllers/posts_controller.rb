5a0e0ab8765425385c57410d4bc8ec55dfd924f2
nyghtowl <warrick.melanie@gmail.com>
2013-07-16 15:13:18 -0700

    Rails app is working where when the button is pushed it posts to Twitter.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
 
 require 'markov_class'
-
+require "rubygems"
+require "twitter"
 
 def create
   @sample_text = open(Rails.root+"lib/markov_class/python_trek.txt").read()
@@ -12,25 +13,22 @@ def create
 end
 
 def user_tweet(random_txt)
-    require "rubygems"
-    require "twitter"
   
-    # Certain methods require authentication. To get your Twitter OAuth credentials,
-    # register an app at http://dev.twitter.com/apps
+    # Twitter authentication
     Twitter.configure do |config|
         config.consumer_key = ENV['TWITTER_KEY']
         config.consumer_secret = ENV['TWITTER_SECRET']
         config.oauth_token = ENV['TWITTER_TOKEN'] 
         config.oauth_token_secret =  ENV['TWITTER_T_SECRET']
     end
-  
+
     # Initialize your Twitter client
-    @twitter = Twitter::Client.new
-  
+    client = Twitter::Client.new
+    
     # Post a status update
-    # @twitter.update(random_txt)
     print random_txt
-    print 'hit controller post'
+    client.update(random_txt)
+    # Twitter.update(random_txt)
     return random_txt
     # redirect_to request.referer, :notice => 'Tweet successfully posted'
 end
